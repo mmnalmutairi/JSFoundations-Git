19d99f79471c958e656dacc09041f8afa330e6e9
Maha Almutairi <carsridder10@gmail.com>
2021-05-24 18:23:10 +0300

    your message here

---
+++ Var.js
@@ -0,0 +1,10 @@
+
+const username = "Eng Maha";
+const number = 30;
+console.log(`The GitHub username is ${username}.`);
+console.log(`${username} has ${number} repositories.`);
+
+let Username = "Maha";
+let Number = 40;
+console.log(`The new GitHub username is ${Username}.`);
+console.log(`${Username} has ${Number + 2} repositories.`);
