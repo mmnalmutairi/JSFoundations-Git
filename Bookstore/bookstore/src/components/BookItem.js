d4ddcfe7df053fc733dd7bffe4e6b2a7382fb88d
Maha Almutairi <carsridder10@gmail.com>
2021-06-09 18:12:10 +0300

    Styling Task

--- Bookstore/bookstore/src/components/BookItem.js
+++ Bookstore/bookstore/src/components/BookItem.js
@@ -1,9 +1,25 @@
+import styled from "styled-components";
+
+const Name = styled.p`
+text-align: center;
+color: #14213d;
+font-size: 20px;
+font-weight: 10px;
+font-family:'Times New Roman', Times, serif;
+`;
+
+const BooksImage = styled.img`
+border-style: solid;
+border-color: #84a59d;
+border-width: 5px;
+height: 300px;
+`;
 
 const BookItem = (props) => {
     return (
         <div className="Book" key={props.id}>
-            <img alt={props.name} className="book-image" src={props.image} />
-            <p className="text">{props.name}</p>
+            <BooksImage alt={props.name} className="book-image" src={props.image} />
+            <Name>{props.name}</Name>
         </div>
 
     );
